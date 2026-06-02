31d7f9d94f84362a245225e862015f59ca4aed45
Auriga1123 <najmacahya05@gmail.com>
2026-06-02 10:44:44 +0700

    Controller untuk modul Supervisor

---
+++ app/Http/Controllers/Supervisor/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers\Supervisor;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaksi;        
+use App\Models\StokBarang;       
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $cabangId = Auth::user()->cabang_id;
+
+        return view('supervisor.dashboard', [
+            'total_transaksi' => Transaksi::where('cabang_id', $cabangId)->count(),
+            'stok_menipis'    => StokBarang::where('cabang_id', $cabangId)
+                                    ->whereColumn('jumlah_stok', '<=', 'stok_minimum')
+                                    ->count(),
+        ]);
+    }
+}
